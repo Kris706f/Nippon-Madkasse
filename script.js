3d00d169a24512e7b6e60a1fe4c14ec04c9cfc1e
Kris706f <126452879+Kris706f@users.noreply.github.com>
2023-09-28 12:52:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,11 @@
 /*Herunder er det som skal bruges til burgermenu*/
 
 const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+
+
 
 menu_btn.addEventListener('click', function(){
     menu_btn.classList.toggle('is-active');
+    mobile_menu.classList.toggle('is-active');
 });
